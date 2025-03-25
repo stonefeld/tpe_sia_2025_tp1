9f0ed2ba2e351dbd793a1fa244d6cbfd9a239d96
tommyfreire <tfreire@pdc.lab>
2025-03-25 16:04:23 -0300

    a* and greedy with manhattan

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import sys
 
 from src.visualizer import SokobanVisualizer
+from src.sokoban import heuristica_manhattan
 
 
 def main():
     file = sys.argv[1]
     algo = sys.argv[2]
+    heuristic = sys.argv[3].lower() if len(sys.argv) >= 4 else None
 
     game = SokobanVisualizer(file)
     if algo == "bfs":
         solution = game.bfs()
     # elif algo == "dfs":
-    #     solution = game.dfs()
-    # elif algo == "greedy":
-    #     solution = game.greedy()
-    # elif algo == "a_star":
-    #     solution = game.a_star()
+    #      solution = game.dfs()
+    elif algo in {"greedy", "astar"}:
+        if heuristic is None:
+            print("Debe especificar una heurística para usar con greedy o astar.")
+            print("Ejemplo: python main.py nivel_1.txt greedy manhattan")
+            sys.exit(1)
+
+        if heuristic == "manhattan":
+            heuristic_fn = heuristica_manhattan
+        # elif heuristic == "..."
+        #     heuristic_fn = ...
+        else:
+            print(f"Heurística no reconocida")
+            sys.exit(1)
+
+        use_astar = algo == "astar"
+        solution = game.informed_search(heuristic_fn, use_astar)
     else:
         raise ValueError("Algoritmo no válido")
 
--- src/sokoban.py
+++ src/sokoban.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import heapq
+from itertools import count
 
 movimientos = {
     "UP": (0, -1),
@@ -97,5 +99,61 @@ class Sokoban:
                     visited.add(new_state)
                     new_node = TreeNode(new_player, new_boxes, move, node)
                     frontier.append(new_node)
+        return None
+    
+    
+    def informed_search(self, heuristic_fn, use_astar: bool):
+        root = TreeNode(self.player, frozenset(self.boxes))
+        frontier = []
+        visited = set()
+        counter = count() # Para desempatar nodos con la misma prioridad y evitar comparar TreeNode vs TreeNode
+
+        # (priority, g(n), TreeNode)
+        h = heuristic_fn(root, self.targets)
+        heapq.heappush(frontier, (h, next(counter), 0, root))
+
+        while frontier:
+            _, _, g, node = heapq.heappop(frontier)
+
+            if all(box in self.targets for box in node.boxes):
+                return node.get_path()
+
+            for move, (dx, dy) in movimientos.items():
+                new_player = (node.player[0] + dx, node.player[1] + dy)
+
+                if self.map[new_player[1]][new_player[0]] == Chars.WALL:
+                    continue
+
+                new_boxes = node.boxes
+
+                if new_player in node.boxes:
+                    new_box = (new_player[0] + dx, new_player[1] + dy)
+
+                    if new_box in node.boxes or self.map[new_box[1]][new_box[0]] == Chars.WALL:
+                        continue
+
+                    new_boxes = frozenset((new_box if box == new_player else box) for box in node.boxes)
+
+                new_state = (new_player, new_boxes)
+
+                if new_state not in visited:
+                    visited.add(new_state)
+                    new_node = TreeNode(new_player, new_boxes, move, node)
+                    g_nuevo = g + 1
+                    h = heuristic_fn(new_node, self.targets)
+                    priority = h if not use_astar else g_nuevo + h
+                    heapq.heappush(frontier, (priority, next(counter), g_nuevo, new_node))
 
         return None
+    
+    
+    
+
+def heuristica_manhattan(node: TreeNode, targets: set[tuple[int, int]]) -> int:
+    total = 0
+    for box in node.boxes:
+        # Calcular la distancia de esta caja a todos los objetivos
+        distancias = [abs(box[0] - goal[0]) + abs(box[1] - goal[1]) for goal in targets]
+        # Tomar la mínima distancia para esta caja
+        total += min(distancias)
+    return total
